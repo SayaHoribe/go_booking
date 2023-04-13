3283b217c3e1c5e1a9d84303d19eeed9c49fe21a
SayaHoribe <s.horibe0831@gmail.com>
2023-04-13 15:47:53 +0900

    first commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"github.com/SayaHoribe/go_booking/pkg/config"
+	"github.com/SayaHoribe/go_booking/pkg/models"
+	"github.com/SayaHoribe/go_booking/pkg/render"
+	"net/http"
+)
+
+// Repo repository used by the handlers
+var Repo *Repository
+
+// Repository the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo create a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the handler for the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the handler for the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
